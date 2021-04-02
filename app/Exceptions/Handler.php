4dc701c3c1ac75d3c7ac8995f5c08f0ade023ee1
Kevin <kevinmachuca96@hotmail.com>
2021-04-02 12:20:36 +0100

    Fixed the unauthenticated case, now return a json with 401 code and an error message.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use App\Http\Controllers\RequestController;
+use Illuminate\Auth\AuthenticationException;
 
 class Handler extends ExceptionHandler
 {
@@ -36,5 +38,10 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return RequestController::returnFail('Internal Server error.', 'Unauthenticated.', 401);
     }
 }
